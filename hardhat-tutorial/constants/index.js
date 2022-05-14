cbdcfbecfd7ddc3741c7788267e86752f295223c
yeshu <673643706@qq.com>
2022-05-14 23:35:19 +0800

    add contract

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xe5680A760129211a90242C9AE52883369a892923";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
